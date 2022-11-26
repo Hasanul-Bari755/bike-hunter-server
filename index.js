831545ed15cd67f510dfb072c1e98f32ce8c0107
Hasanul Bari <hasanulbari8@gmail.com>
2022-11-27 04:47:32 +0600

    verify done

--- index.js
+++ index.js
@@ -193,6 +193,30 @@ async function run() {
       );
       res.send(result);
     });
+
+    app.delete("/myproduct/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await productCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    app.patch("/verify", async (req, res) => {
+      const email = req.query.email;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updatedDoc = {
+        $set: {
+          verifystatus: "verified",
+        },
+      };
+      const result = await productCollection.updateMany(
+        filter,
+        updatedDoc,
+        options
+      );
+      res.send(result);
+    });
   } finally {
   }
 }
